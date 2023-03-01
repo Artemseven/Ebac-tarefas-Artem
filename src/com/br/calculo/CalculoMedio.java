5496261ff611d9491819d1e58c2e3b95c53eba49
Artemseven <artemdobrasil@gmail.com>
2023-02-28 23:40:41 -0300

    mod8

---
+++ src/com/br/calculo/CalculoMedio.java
@@ -0,0 +1,30 @@
+package com.br.calculo;
+
+import java.util.Scanner;
+
+public class CalculoMedio {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com a primeira nota1");
+		double nota1 = scan.nextDouble();
+		
+		System.out.println("Entre com a primeira nota2");
+		double nota2 = scan.nextDouble();
+		
+		System.out.println("Entre com a primeira nota3");
+		double nota3 = scan.nextDouble();
+		
+		System.out.println("Entre com a primeira nota4");
+		double nota4 = scan.nextDouble();
+		
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("A media e :" + media);
+		
+
+	}
+
+}
